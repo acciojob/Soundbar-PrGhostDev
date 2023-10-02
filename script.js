a1d462a6e1d586738a432ff14c251f5d8dd31d73
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 16:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const soundName = button.id;
+        playSound(soundName);
+    });
+});
+
+stopButton.addEventListener('click', () => {
+    stopAllSounds();
+});
+
+function playSound(name) {
+    const audio = new Audio(`./sounds/${name}.mp3`);
+    audio.play();
+}
+
+function stopAllSounds() {
+    const audioElements = document.querySelectorAll('audio');
+    audioElements.forEach((audio) => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
